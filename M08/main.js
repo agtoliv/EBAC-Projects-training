f69bcad12b86fee49e249b8397b4d3a22d6805b1
Augusto Oliveira <augustooliveira.silveira@gmail.com>
2024-12-16 15:38:00 -0300

    add m8

---
+++ M08/main.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form-principal");
+const nome = document.getElementById("nome");
+const btn = document.getElementById("btn");
+
+form.addEventListener('submit'
+    //colcoar as funções
+);
+
+function nomeCompleto(){
+    form = form.includes(innerHTML(" "))? btn.disabled() : btn.avaliable();
+}
